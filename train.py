be0748022bf6d44e7843df2415132d9db15e4a9a
huiyulin25 <39060530+huiyulin25@users.noreply.github.com>
2018-05-07 22:11:27 -0400

    Update train.py

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ seed(1)
 from tensorflow import set_random_seed
 set_random_seed(2)
 
-
+# Set batch-size
 batch_size = 32
 
 #Prepare input data
@@ -36,7 +36,7 @@ img_size = 128
 num_channels = 3
 train_path='training_data'
 
-# We shall load all the training and validation images and labels into memory using openCV and use that during training
+# Load training data, validation data, and labels
 data = dataset.read_train_sets(train_path, img_size, classes, validation_size=validation_size)
 
 
@@ -55,7 +55,7 @@ y_true_cls = tf.argmax(y_true, dimension=1)
 
 
 
-##Network graph params
+# Network graph parameters
 filter_size_conv1 = 3 
 num_filters_conv1 = 32
 
@@ -67,9 +67,10 @@ num_filters_conv3 = 64
     
 fc_layer_size = 128
 
+# Define weight
 def create_weights(shape):
     return tf.Variable(tf.truncated_normal(shape, stddev=0.05))
-
+# Define bias
 def create_biases(size):
     return tf.Variable(tf.constant(0.05, shape=[size]))
 
@@ -80,9 +81,9 @@ def create_convolutional_layer(input,
                conv_filter_size,        
                num_filters):  
     
-    ## We shall define the weights that will be trained using create_weights function.
+    ## initiate weight
     weights = create_weights(shape=[conv_filter_size, conv_filter_size, num_input_channels, num_filters])
-    ## We create biases using the create_biases function. These are also trained.
+    ## initiate bias
     biases = create_biases(num_filters)
 
     ## Creating the convolutional layer
@@ -93,12 +94,12 @@ def create_convolutional_layer(input,
 
     layer += biases
 
-    ## We shall be using max-pooling.  
+    ## Max-pooling  
     layer = tf.nn.max_pool(value=layer,
                             ksize=[1, 2, 2, 1],
                             strides=[1, 2, 2, 1],
                             padding='SAME')
-    ## Output of pooling is fed to Relu which is the activation function for us.
+    ## Activate ReLu
     layer = tf.nn.relu(layer)
 
     return layer
@@ -106,14 +107,12 @@ def create_convolutional_layer(input,
     
 
 def create_flatten_layer(layer):
-    #We know that the shape of the layer will be [batch_size img_size img_size num_channels] 
-    # But let's get it from the previous layer.
+    # Get the shape of the layer [batch_size img_size img_size num_channels] from the previous layer.
     layer_shape = layer.get_shape()
 
-    ## Number of features will be img_height * img_width* num_channels. But we shall calculate it in place of hard-coding it.
     num_features = layer_shape[1:4].num_elements()
 
-    ## Now, we Flatten the layer so we shall have to reshape to num_features
+    # Flatten the layer and reshape it
     layer = tf.reshape(layer, [-1, num_features])
 
     return layer
@@ -124,18 +123,19 @@ def create_fc_layer(input,
              num_outputs,
              use_relu=True):
     
-    #Let's define trainable weights and biases.
+    # define weights and biases.
     weights = create_weights(shape=[num_inputs, num_outputs])
     biases = create_biases(num_outputs)
 
-    # Fully connected layer takes input x and produces wx+b.Since, these are matrices, we use matmul function in Tensorflow
+    # Fully connected layer takes input x and produces wx+b. 
     layer = tf.matmul(input, weights) + biases
+    # Add ReLu
     if use_relu:
         layer = tf.nn.relu(layer)
 
     return layer
 
-
+# Network Design
 layer_conv1 = create_convolutional_layer(input=x,
                num_input_channels=num_channels,
                conv_filter_size=filter_size_conv1,
